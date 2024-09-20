988a0ddd255cbc88ab92229b3a5aa7e783ccc9b9
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2024-09-21 02:16:03 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React ,{useState}from 'react'
-import Products from "./Products";
+import React,{useState} from 'react'
+import './App.css';
 
-function App() {
-const[state,setState]=useState('');
-const[data,setData]=useState([])
-const YOUR_APP_ID = "3d422a92";
-const YOUR_APP_KEY = "febe43c7c4cafa702c22226f9683d8ae"
-const submitHandler=(e)=>{
-    e.preventDefault();
-
-    fetch(`https://api.edamam.com/search?q=${state}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
-        response => response.json()
-    ).then(
-        data => setData(data.hits)
-    )
+const App = () => {
   
-}
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+  const changeHandler = e =>{
+    setCity(e.target.value);
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
+  }
 
   return (
-    <div><center>
-<form onSubmit={submitHandler}>
-        <h3>FOOD RECIPE APPLICATION</h3>
-        <input type="text" value={state} onChange={(e)=>setState(e.target.value)}/>
-      <input type="submit" className="btn btn-primary" value="search"/>
-      
-      </form>
-     <Products data={data} />
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather App</h4>
+            <form onSubmit={submitHandler}>
+              <input size="30" type="text" name="city" onChange={changeHandler} value={city}/> <br /><br />
+              <input type="submit" className = "btn btn-primary"value="Get Temperature" />
+            </form><br /> <br />
+            <div>
+               <h1>{result}</h1> 
+            </div>
+          </div>
+        </div>
       </center>
     </div>
   )
