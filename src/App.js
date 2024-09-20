47f0516756fc7efbc7328ba9e1a7810eab0b98bc
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2024-09-21 01:34:14 +0530

    1st one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState}from 'react'
+import Products from "./Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const[state,setState]=useState('');
+const[data,setData]=useState([])
+const YOUR_APP_ID = "3d422a92";
+const YOUR_APP_KEY = "febe43c7c4cafa702c22226f9683d8ae"
+const submitHandler=(e)=>{
+    e.preventDefault();
+
+    fetch(`https://api.edamam.com/search?q=${state}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
+        response => response.json()
+    ).then(
+        data => setData(data.hits)
+    )
+  
 }
 
-export default App;
+  return (
+    <div><center>
+<form onSubmit={submitHandler}>
+        <h3>FOOD RECIPE APPLICATION</h3>
+        <input type="text" value={state} onChange={(e)=>setState(e.target.value)}/>
+      <input type="submit" className="btn btn-primary" value="search"/>
+      
+      </form>
+     <Products data={data} />
+      </center>
+    </div>
+  )
+}
+
+export default App
